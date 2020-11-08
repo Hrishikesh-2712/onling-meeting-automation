557932eb8a7125d86163b3854eff299e027d3f90
Hrishikesh-2712 <74128509+Hrishikesh-2712@users.noreply.github.com>
2020-11-08 17:04:02 +0530

    Update routine.py

--- routine.py
+++ routine.py
@@ -1,18 +1,18 @@
-#f=with(open('routine.csv','r'))
-from datetime import datetime
-a=datetime.now().strftime('%H:%M')
+#This is an example
+
 def timings():
-    return ('08:00','09:00','09:53','09:40','11:41',a)
+    return ('08:00','09:00','09:53','09:40','11:41',t,'18:00')
 
 def meetids():
-    return ('73263794809','73955949894','7462732282','5487512685','meet.google.com','meet.google.com')
+    return ('73263794809','73955949894','7462732282','5487512685','meet.google.com','meet.google.com','94546282737')
 
 def passwords():
-    return ('Organic','3z70kv','dks654','dhdr66','nwj-tusp-sir','fxd-vffe-xuw')
+    return ('Organic','3z70kv','dks654','dhdr66','nwj-tusp-sir','fxd-vffe-xuw','vN4b8j')
 
 def subjects():
-    return ('Chemistry Tuition','Chemistry School','Physics','Maths','Computer','Trail')
+    return ('Chemistry Tuition','Chemistry School','Physics','Maths','Computer','Trail','Trail')
 
 
 def durations():
-    return (2400,2400,2400,2400,2400,2400)
+    return (2400,2400,2400,2400,2400,2400,100)
+    #Enter duration in seconds
