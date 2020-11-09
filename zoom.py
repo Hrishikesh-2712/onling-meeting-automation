31ea38446fc543fa8c755f05beb7ecb2006eb509
Hrishikesh-2712 <74128509+Hrishikesh-2712@users.noreply.github.com>
2020-11-09 14:44:11 +0530

    Update zoom.py

--- zoom.py
+++ zoom.py
@@ -4,31 +4,41 @@ import execute
 from datetime import datetime
 
 def join(meetingid,password,subject,duration):
-    execute.startzoom()
+    f=open('log.txt','a')
     #join a meeting
     execute.startzoom()
-    print(datetime.now(),'launching zoom...')
-    time.sleep(5)
+    n=datetime.now().strftime('%H:%M:%S %D')+'launched zoom\n'
+    f.write(n)
+    time.sleep(6)
     pyautogui.press('tab')
     pyautogui.press('enter')
-    print(datetime.now(), 'joining',subject,'class')
+    print(datetime.now().strftime('%H:%M:%S %D'), 'joining',subject,'class')
+    n=datetime.now().strftime('%H:%M:%S %D')+'joining'+subject+'class\n'
+    f.write(n)
+    
 
     #enter meeting id
-    time.sleep(5)
-    print(datetime.now(),'entering id...')
+    time.sleep(6)
+    print(datetime.now().strftime('%H:%M:%S %D'),'entering id...')
+    n=datetime.now().strftime('%H:%M:%S %D')+'entering id...\n'
+    f.write(n)
     pyautogui.write(meetingid)
 
     #join
     pyautogui.press('enter')
-    time.sleep(5)
+    time.sleep(6)
 
     #meeting password
     pyautogui.write(password)
-    print(datetime.now(),'entering password...')
+    print(datetime.now().strftime('%H:%M:%S %D'),'entering password...')
+    n=datetime.now().strftime('%H:%M:%S %D')+'entering password...\n'
+    f.write(n)
 
     #join meeting
     pyautogui.press('enter')
-    print(datetime.now(),'entering class.')
+    print(datetime.now().strftime('%H:%M:%S %D'),'entering class.')
+    n=datetime.now().strftime('%H:%M:%S %D')+'entering class.\n'
+    f.write(n)
 
     #join audio
     time.sleep(30)
@@ -37,10 +47,20 @@ def join(meetingid,password,subject,duration):
     y=y/2.4
     pyautogui.moveTo(x,y)
     pyautogui.click(button='left', clicks=3)
+    n=datetime.now().strftime('%H:%M:%S %D')+'joined audio\n'
+    f.write(n)
 
     #waiting for class to over
+    n=datetime.now().strftime('%H:%M:%S %D')+f'going to sleep for {duration} seconds\n'
+    f.write(n)
     time.sleep(duration)
+    print(datetime.now().strftime('%H:%M:%S %D'),'time over')
+    n=datetime.now().strftime('%H:%M:%S %D')+'time over\n'
+    f.write(n)
 
     #closing zoom
     execute.closezoom()
-    print(datetime.now(),'left class\n\n waiting of next class to start')
+    n=datetime.now().strftime('%H:%M:%S %D')+'left class\n\n waiting of next class to start\n'
+    f.write(n)
+    f.close()
+    print(datetime.now().strftime('%H:%M:%S %D'),'left class\n\n waiting of next class to start')
