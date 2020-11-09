eb5f5a0e4f9dbf08f7df1266618e0fc60ee000bd
Hrishikesh-2712 <74128509+Hrishikesh-2712@users.noreply.github.com>
2020-11-09 14:42:12 +0530

    Update routine.py

--- routine.py
+++ routine.py
@@ -1,18 +1,19 @@
 #This is an example
 
+
 def timings():
-    return ('08:00','09:00','09:53','09:40','12:20','15:30','18:00')
+    return ('08:00','09:00','09:50','10:40','12:20','14:32','14:35')
 
 def meetids():
-    return ('73263794809','73955949894','7462732282','5487512685','meet.google.com','meet.google.com','94546282737')
+    return ('73263794809','75231310465','7462732282','5487512685','meet.google.com','meet.google.com','98036959341')
 
 def passwords():
-    return ('Organic','3z70kv','dks654','dhdr66','nwj-tusp-sir','fxd-vffe-xuw','vN4b8j')
+    return ('Organic','2DyBqp','dks654','dhdr66','nwj-tusp-sir','fxd-vffe-xuw','ySE7BT')
 
 def subjects():
-    return ('Chemistry Tuition','Chemistry School','Physics','Maths','Computer','Trail','Trail')
+    return ('Chemistry Tuition','Chemistry School','Physics','Maths','Computer','Gmeet Trail','Zoom Trail')
 
 
 def durations():
-    return (2400,2400,2400,2400,2400,2400,100)
+    return (2700,2700,2700,2700,2700,15,15)
     #Enter duration in seconds
