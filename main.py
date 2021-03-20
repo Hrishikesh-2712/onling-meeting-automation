0295ec9002bcaf2bfb2038d17253d991008c16ab
Hrishikesh-2712 <74128509+Hrishikesh-2712@users.noreply.github.com>
2021-03-20 21:32:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,191 +1,229 @@
-from datetime import datetime
 from time import sleep
-import os, getpass, csv, webbrowser, pyautogui
+from datetime import datetime
+from selenium import webdriver
+import csv, getpass, os, pyautogui, requests
+from selenium.webdriver.chrome.options import Options
 
-user=getpass.getuser()          # get current user name
-folder='' 
-date=''
-#gmeet
-def joinmeet(end_time,meetid,password,subject,screenshot):
-    a=rf'https:{meetid}/{password}'
-    webbrowser.open(a)
+user = getpass.getuser()  # get current user name
+folder = ''
+date = ''
+
+
+# gmeet
+def joinmeet(end_time, meetid, password, subject, screenshot):
+    a = rf'https:{meetid}/{password}'
+
     print(datetime.now().strftime('%H:%M:%S %D'), 'launched browswe')
 
-    sleep(20)
+    join_meet(password)
 
-    #mute mic
-    pyautogui.hotkey('ctrl','d')
-    print(datetime.now().strftime('%H:%M:%S %D'), 'mic off')
-    sleep(1)
-
-    #turn off cam
-    pyautogui.hotkey('ctrl','e')
-    print(datetime.now().strftime('%H:%M:%S %D'), 'cam off')
-    
-    #join
-    sleep(2)
-    
-    x,y=pyautogui.size()
-    x/=2
-    y/=2
-    '''pyautogui.click(x,y)'''
-    for loop in range (6):
-        pyautogui.press('tab')
-        sleep(0.3)
-    pyautogui.press('enter')
-    print(datetime.now().strftime('%H:%M:%S %D'), 'joining',subject,'class')
-
-    #fullscreen entered
-    pyautogui.moveTo(x,y)
-    pyautogui.click(clicks=2)
-    
-    ending(screenshot,row[1],subject)
+    ending(screenshot, row[1], subject)
+    home()
     print('qutting')
+    print(datetime.now().strftime('%H:%M:%S %D'), 'left class\n\n waiting of next class to start')
 
-    #left full screen
-    pyautogui.moveTo(x,y)
-    pyautogui.click(clicks=2)
-    
-    #for hang up either use
 
-    ''' this'''
-    x,y=pyautogui.size()
-    x/=2
-    y1=y/1.129411764705882
-    y/=2
-    pyautogui.moveTo(x, y+100, 2, pyautogui.easeOutQuad)
-    pyautogui.moveTo(x,y1)
+def gmail_login(mail_address, password):
+    global logged
+    global driver
+
+    opt = Options()
+    opt.add_argument('--disable-blink-features=AutomationControlled')
+    opt.add_argument('--start-maximized')
+    opt.add_experimental_option("prefs", {
+        "profile.default_content_setting_values.media_stream_mic": 1,
+        "profile.default_content_setting_values.media_stream_camera": 1,
+        "profile.default_content_setting_values.geolocation": 0,
+        "profile.default_content_setting_values.notifications": 1})
+
+    driver = webdriver.Chrome(options=opt)
+
+    # Login Page 
+    driver.get(f'https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://meet.google.com')
+    # input Gmail 
+    driver.find_element_by_id("identifierId").send_keys(mail_address)
+    driver.find_element_by_id("identifierNext").click()
+    driver.implicitly_wait(100)
+
+    # input Password 
+    driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
+    driver.implicitly_wait(100)
+    driver.find_element_by_id("passwordNext").click()
+    driver.implicitly_wait(100)
+    driver.minimize_window()
     sleep(2)
-    pyautogui.click()
+
+
+def join_meet(meetid):
+    # turn off Microphone
+    global driver
+    driver.get(f'https://meet.google.com/{meetid}')
+    driver.maximize_window()
+    driver.implicitly_wait(1000)
     sleep(2)
+    driver.find_element_by_css_selector(
+        '#yDmH0d > c-wiz > div > div > div:nth-child(9) > div.crqnQb > div > div > div.vgJExf > div > div.KieQAe > '
+        'div.ZUpb4c > div.oORaUb.NONs6c > div > div.EhAUAc > div.ZB88ed > div > div > div > span > span > div > '
+        'div.IYwVEf.HotEze.uB7U9e.nAZzG > div > svg').click()
+    driver.implicitly_wait(3000)
 
-    '''
+    # turn off camera 
+    sleep(1)
+    driver.find_element_by_css_selector(
+        '#yDmH0d > c-wiz > div > div > div:nth-child(9) > div.crqnQb > div > div > div.vgJExf > div > div.KieQAe > '
+        'div.ZUpb4c > div.oORaUb.NONs6c > div > div.EhAUAc > div.GOH7Zb > div > div > span > span > div > div > '
+        'svg').click()
+    driver.implicitly_wait(3000)
 
-    #or         it's not performing well
-         
-    #left full screen
-    pyautogui.moveTo(x,y)
-    pyautogui.click(clicks=2)
-    sleep(2)
-    for loop in range (7):            #this one to hang up
-        pyautogui.press('tab')
-        sleep(0.3)
-    pyautogui.press('enter')   in the next release I'll use selenuim stay tuned ;)'''
-    pyautogui.hotkey('alt','tab')
-    print(datetime.now().strftime('%H:%M:%S %D'),'left class\n\n waiting of next class to start')
+    # Join meet
+    sleep(5)
+    driver.implicitly_wait(2000)
+    driver.find_element_by_css_selector('div.uArJ5e.UQuaGc.Y5sE8d.uyXBBb.xKiqt').click()
+    pyautogui.press('f11')
 
-    
-#zoom
-def joinzoom(end_time,meetingid,password,subject,screenshot='y'):
-    #start zoom
-    os.system(r"TASKKILL /F /IM zoom.exe") # if previous class was running it would be closed
-    os.startfile(rf"C:/Users/{user}/AppData/Roaming/Zoom/bin/Zoom.exe")    
-    
-    #join a meeting
+
+def home():
+    global driver
+    driver.get('https://meet.google.com')
+    driver.implicitly_wait(60)
+    sleep(1)
+    pyautogui.press('f11')
+    sleep(1)
+    driver.minimize_window()
+
+
+def launch_zoom():
+    try:
+        os.startfile(rf"C:/Users/{user}/AppData/Roaming/Zoom/bin/Zoom.exe")
+    except FileNotFoundError:
+        print('zoom software is not installed, please wait till installation')
+        print('Download Starting...')
+
+        url = 'https://www.zoom.us/client/latest/ZoomInstaller.exe'
+        r = requests.get(url)
+        filename = url.split('/')[-1]
+        with open(filename, 'wb') as output_file:
+            output_file.write(r.content)
+        print('Download Completed!!!')
+        os.startfile(os.getcwd() + '\\' + '\\' + filename)
+        sleep(15)
+        os.startfile(rf"C:/Users/{user}/AppData/Roaming/Zoom/bin/Zoom.exe")
+
+
+# zoom
+def joinzoom(end_time, meetingid, password, subject, screenshot='y'):
+    # start zoom
+    os.system(r"TASKKILL /F /IM zoom.exe")  # if previous class was running it would be closed
+
+    launch_zoom()
+
+    print('starting')
+    # join a meeting
     sleep(12)
     pyautogui.press('tab')
     pyautogui.press('enter')
-    print(datetime.now().strftime('%H:%M:%S %D'), 'joining',subject,'class')
-    
+    print(datetime.now().strftime('%H:%M:%S %D'), 'joining', subject, 'class')
 
-    #enter meeting id
+    # enter meeting id
     sleep(6)
-    print(datetime.now().strftime('%H:%M:%S %D'),'entering id...')
+    print(datetime.now().strftime('%H:%M:%S %D'), 'entering id...')
     pyautogui.write(meetingid)
 
-    #join
+    # join
     pyautogui.press('enter')
     sleep(6)
 
-    #meeting password
+    # meeting password
     pyautogui.write(password)
-    print(datetime.now().strftime('%H:%M:%S %D'),'entering password...')
+    print(datetime.now().strftime('%H:%M:%S %D'), 'entering password...')
 
-    #join meeting
+    # join meeting
     pyautogui.press('enter')
-    print(datetime.now().strftime('%H:%M:%S %D'),'entering class.')
+    print(datetime.now().strftime('%H:%M:%S %D'), 'entering class.')
 
-    #join audio
+    # join audio
     sleep(15)
-    x,y=pyautogui.size()
-    x=x/2.2
-    y=y/2.4
-    pyautogui.moveTo(x,y)
+    x, y = pyautogui.size()
+    x = x / 2.2
+    y = y / 2.4
+    pyautogui.moveTo(x, y)
     pyautogui.click(button='left', clicks=3)
 
+    ending(screenshot, end_time, subject)
 
-    ending(screenshot,end_time,subject)
+    print(datetime.now().strftime('%H:%M:%S %D'), 'time over')
 
-    print(datetime.now().strftime('%H:%M:%S %D'),'time over')
-
-    #closing zoom
-    os.system(r"TASKKILL /F /IM zoom.exe")
-    print(datetime.now().strftime('%H:%M:%S %D'),'left class\n\n waiting of next class to start')
+    # closing zoom
+    os.popen("TASKKILL /F /IM zoom.exe")
+    print(datetime.now().strftime('%H:%M:%S %D'), 'left class\n\n waiting of next class to start')
 
 
-def ending(screenshot,end_time,sub):
-    h=int(end_time[:2])
-    m=int(end_time[3:5])
-    if screenshot=='n': # sleeping till class gets over
+def ending(screenshot, end_time, sub):
+    h = int(end_time[:2])
+    m = int(end_time[3:5])
+    if screenshot == 'n':  # sleeping till class gets over
         print('waiting for class to over')
-        print('end time',end_time)
-        t=(h*3600)+(m*60)
-        ch=int(datetime.now().strftime('%H'))
-        cm=int(datetime.now().strftime('%M'))
-        s=int(datetime.now().strftime('%S'))
-        ct=(ch*3600)+(cm*60)+s
-        e=t-ct
-        print('calculated',e,'s')
+        print('end time', end_time)
+        t = (h * 3600) + (m * 60)
+        ch = int(datetime.now().strftime('%H'))
+        cm = int(datetime.now().strftime('%M'))
+        s = int(datetime.now().strftime('%S'))
+        ct = (ch * 3600) + (cm * 60) + s
+        e = abs(t - ct)
+        print('calculated', e, 's')
         sleep(e)
         print('out')
     else:
-        shots(screenshot,h,m,sub)
+        shots(screenshot, h, m, sub)
 
 
-#taking screenshots
-def shots(s,h,m,sub):
-    check(sub) #creating folders
+# taking screenshots
+def shots(s, h, m, sub):
+    check(sub)  # creating folders
     global folder
     print('taking screenshots')
-    while h >int(datetime.datetime.now().strftime('%H')) or h ==int(datetime.datetime.now().strftime('%H')) and m >int(datetime.datetime.now().strftime('%M')):
-        now=datetime.now().strftime('%H %M %S %d %M %Y')
+    while h > int(datetime.now().strftime('%H')) or h == int(datetime.now().strftime('%H')) and m > int(
+            datetime.now().strftime('%M')):
+        now = datetime.now().strftime('%H %M %S %d %M %Y')
         pyautogui.screenshot().save(f'C://Users//{user}//Pictures//Screenshots//{sub}//{folder}//{now}.png')
-        print(now,'took a screenshot')
+        print(now, 'took a screenshot')
         sleep(s)
-        
-    print(f'Screenshots for this class was saved in the following directory ->\n C://Users//{user}//Pictures//Screenshots//{sub}//{date} \n')
 
-#creating folder
+    print(f'Screenshots for this class was saved in the following directory ->\n C://Users//{user}//Pictures'
+          f'//Screenshots//{sub}//{date} \n')
+
+
+# creating folder
 def check(sub):
-    sub=row[4]
+    sub = row[4]
     global date
     global folder
-    base_dir=f'C://Users//{user}//Pictures//'
+    base_dir = f'C://Users//{user}//Pictures//'
     if os.path.isdir(base_dir):
         print('good going')
-        if os.path.isdir(base_dir+r'Screenshots//'):
+        if os.path.isdir(base_dir + r'Screenshots//'):
             print('keep up')
-            if os.path.isdir(base_dir+f'Screenshots//{sub}//'):
+            if os.path.isdir(base_dir + f'Screenshots//{sub}//'):
                 print('alright')
-                now=datetime.now().strftime('%H %M %d %M %Y')
-                if os.path.isdir(base_dir+f'Screenshots//{sub}//{now}//'):
+                now = datetime.now().strftime('%H %M %d %M %Y')
+                if os.path.isdir(base_dir + f'Screenshots//{sub}//{now}//'):
                     print('check passed')
-                    folder=now
+                    folder = now
                     return True
                 else:
-                    now=datetime.now().strftime('%H %M %d %M %Y')
-                    os.mkdir(base_dir+f'Screenshots//{sub}//{now}')
+                    now = datetime.now().strftime('%H %M %d %M %Y')
+                    os.mkdir(base_dir + f'Screenshots//{sub}//{now}')
                     check(sub)
             else:
-                os.mkdir(base_dir+f'Screenshots//{sub}//')
+                os.mkdir(base_dir + f'Screenshots//{sub}//')
                 check(sub)
         else:
-            os.mkdir(base_dir+'Screenshots//')
+            os.mkdir(base_dir + 'Screenshots//')
             check(sub)
     else:
         os.mkdir(base_dir)
         check(sub)
-        
+
 
 '''screenshots
     check()
@@ -194,6 +232,8 @@ def check(sub):
     minute=48-1
     shots(intervel,hour,minute)
 '''
+
+
 def se(a):
     if 'y' in a:
         return 10
@@ -203,25 +243,30 @@ def se(a):
         return int(a)
     else:
         return 10
-    
 
-print(datetime.now().strftime('%H:%M:%S %D'),'reading routine file for upcoming class')
 
+print('loading...')
+
+mail_address = 'jackoggy.cn18@gmail.com'
+pas = 'oggyjackcn18'
+gmail_login(mail_address, pas)
+print('loaded')
+os.system('cls')
+print(datetime.now().strftime('%H:%M:%S %D'), 'reading routine file for upcoming class')
 
 # starter
 while True:
-    with open('routine.csv','r') as f:
-        c=csv.reader(f)
+    with open('routine.csv', 'r') as f:
+        c = csv.reader(f)
         for row in c:
-            now=datetime.now().strftime('%H:%M')
+            now = datetime.now().strftime('%H:%M')
             if row[0] == now:
-                print(now,'in timings')
-                if row[2]=='meet.google.com':
-                    joinmeet(row[1],row[2],row[3],row[4],se(row[5]))
+                print(now, 'in timings')
+                if row[2] == 'meet.google.com':
+                    joinmeet(row[1], row[2], row[3], row[4], se(row[5]))
                 elif row[2].isdigit:
-                    joinzoom(row[1],row[2],row[3],row[4],se(row[5]))
-                else :
+                    joinzoom(row[1], row[2], row[3], row[4], se(row[5]))
+                else:
                     print('Please correct routine info data')
-        ''' print(row[4],type(row),type(row[4])) '''
         f.close()
         sleep(2)
